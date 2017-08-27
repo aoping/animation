ae64df2851e4181f6269ff91c407e86ac1bd1635
zoro <aoping@yougola.com>
2017-08-27 10:29:13 +0800

    webpack-merge

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,20 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
+
 const PATHS = {
     app: path.join(__dirname, 'app'),
     build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+
+// module.exports = { 
+
+
+const common = {
+
     // Entry accepts a path or an object of entries.
     // We'll be using the latter form given it's
     // convenient with more complex configurations.
@@ -23,3 +31,17 @@ module.exports = {
         })
     ]
 };
+
+
+var config;
+
+// Detect how npm is run and branch based on that
+switch (process.env.npm_lifecycle_event) {
+    case 'build':
+        config = merge(common, {});
+        break;
+    default:
+        config = merge(common, {});
+}
+
+module.exports = config;
