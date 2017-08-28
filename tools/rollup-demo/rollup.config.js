1fcfcae13a0af9540f3c91f1e684238471e2fca3
zoro <aoping@yougola.com>
2017-08-28 15:27:18 +0800

    rollup 单独打包

--- tools/rollup-demo/rollup.config.js
+++ tools/rollup-demo/rollup.config.js
@@ -4,6 +4,13 @@ import resolve from 'rollup-plugin-node-resolve';
 export default {
     entry: 'src/main.js',
     format: 'cjs',
-    plugins: [resolve()],
+    plugins: [resolve({
+        // 给 resolve 插件传入自定配置
+        customResolveOptions: {
+            moduleDirectory: 'node_modules'
+        }
+    })],
+    // 指明哪个模块被看作外部引用(external)
+    external: ['lodash'],
     dest: 'bundle.js'
 };
--- tools/rollup-demo/src/main.js
+++ tools/rollup-demo/src/main.js
@@ -1,5 +1,6 @@
 // import { version } from '../package.json';
 import answer from 'the-answer';
+import _ from 'lodash';
 export default function() {
     console.log('version ' + version);
     console.log('the answer is ' + answer);
