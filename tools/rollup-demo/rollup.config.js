639aeeb6b30f376bc1b7b0c95666d6e3aa70ba14
zoro <aoping@yougola.com>
2017-08-28 15:33:03 +0800

    rollup babel

--- tools/rollup-demo/rollup.config.js
+++ tools/rollup-demo/rollup.config.js
@@ -1,15 +1,19 @@
 // import json from 'rollup-plugin-json';
 import resolve from 'rollup-plugin-node-resolve';
-
+import babel from 'rollup-plugin-babel';
 export default {
     entry: 'src/main.js',
     format: 'cjs',
     plugins: [resolve({
-        // 给 resolve 插件传入自定配置
-        customResolveOptions: {
-            moduleDirectory: 'node_modules'
-        }
-    })],
+            // 给 resolve 插件传入自定配置
+            customResolveOptions: {
+                moduleDirectory: 'node_modules'
+            }
+        }),
+        babel({
+            exclude: 'node_modules/**' // 仅仅转译我们的源码
+        })
+    ],
     // 指明哪个模块被看作外部引用(external)
     external: ['lodash'],
     dest: 'bundle.js'
--- tools/rollup-demo/src/main.js
+++ tools/rollup-demo/src/main.js
@@ -1,7 +1,10 @@
 // import { version } from '../package.json';
 import answer from 'the-answer';
 import _ from 'lodash';
-export default function() {
-    console.log('version ' + version);
-    console.log('the answer is ' + answer);
+// export default function() {
+//     console.log('version ' + version);
+//     console.log('the answer is ' + answer);
+// }
+export default () => {
+    console.log(`the answer is ${answer}`);
 }
