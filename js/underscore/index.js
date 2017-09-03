3b77e62a12c9fd48f1ca3a0c1ab71d2a17bf72d9
zoro <aoping@yougola.com>
2017-09-03 21:22:00 +0800

    underscore reduce

--- js/underscore/index.js
+++ js/underscore/index.js
@@ -1,16 +1,16 @@
 const _ = require('./underscore')
 
 // var underscore = _.noConflict();
-// var score = underscore.map([1, 2, 3], function(item) {
-//     return item * 2
-// })
-// console.log(score)
+var score = underscore.map([1, 2, 3], function(item) {
+    return item * 2
+})
+console.log(score)
 
 var results = _.map([1, 2, 3], function(elem) {
     return elem * 2;
 }); // => [2,4,6]
 
-
-
-
-console.log(results)
+var array = [1, 2, 3, 4, 5];
+var sum = _.reduce(array, function(prev, current) {
+    return prev + current;
+}, 0);
